c8185e5f52ea09269efed34fc0fa8db7df85dfac
TheCheeseMeister <ssomething511@gmail.com>
2024-07-27 19:59:06 -0400

    Added task layout in main content area. You can click on a task to show the description of the task. Isn't smooth but that's due to visibility not working well with transition.

--- src/index.js
+++ src/index.js
@@ -9,31 +9,56 @@ const projects = [];
 const addTask = document.getElementById('addTask');
 const addForm = document.getElementById('addForm');
 
+// Dropdown menu
+const dropdown = document.getElementById('selectProj');
+
+// Add task - sidebar button
 const addTaskButton = document.getElementById('addTaskButton');
-addTaskButton.addEventListener("click", () => {
+addTaskButton.addEventListener("click", (event) => {
+    event.preventDefault();
+    // Updates dropdown menu before showing
+    var str = "";
+    for (let project of projects) {
+        str += `<option>${project.title}</option>`;
+    }
+    dropdown.innerHTML = str;
+
     addTask.showModal();
 });
 
 // Add Task Form Button
-addTask.children[0].children[2].addEventListener("click", (event) => {
+
+addTask.children[0].children[3].addEventListener("click", (event) => {
     event.preventDefault();
     addTask.close();
 
-    const formData = new FormData(addForm, addTask.children[0].children[2]);
+    //const formData = new FormData(addForm, addTask.children[0].children[2]);
     // formData.get('title');
     // addTaskToProject(project, formdata stuff);
     // need dropdown for existing projects
+    addForm.reset();
 });
 
-function addNewProject() {
-    projects.push(createProject("Family"));
+// Adding new projects
 
-    /*const temp = document.createElement("button");
-    temp.textContent = projects[0].title;
+function addNewProject(text) {
+    projects.push(createProject(text));
+
+    const temp = document.createElement("button");
+    temp.textContent = text
     
-    sidebar.appendChild(temp);*/
+    sidebar.children[2].appendChild(temp);
 }
 
 addNewProject("Family");
-
 addTaskToProject(projects[0], "Brush Teeth", "Really get in there", "3 Minutes", "NOW");
+
+addNewProject("Work");
+addTaskToProject(projects[1], "Finish ToDo List", "It's important", "yeah", "mhm");
+
+const testingTask = document.getElementById('task');
+const testingTaskDesc = document.getElementById('task-desc');
+
+testingTask.addEventListener("click", () => {
+    testingTaskDesc.classList.toggle("selected");
+});
